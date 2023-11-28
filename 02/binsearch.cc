b467bcdb373121550b554481ac882a7969c7c3de
Igor Dimitrov <igor-dimi@protonmail.com>
2023-11-28 12:01:38 +0100

    added binary search algorithm to 02

---
+++ 02/binsearch.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define N 100
+
+int main()
+{
+    int a[N];
+    int n, b;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", a+i) ;
+    scanf("%d", &b);
+
+    int i = 0;
+    int j = n - 1;
+   //Invariant: b possibly in a[i ... j]
+   while (b != a[(i + j)/2] && i != j){
+       if (b > a[(i + j) / 2]) i = (i + j)/2 + 1;
+       else j = (i + j)/2 - 1;
+   }
+   //Post-condition: b possibly in a[i ... j] && (b == a[(i + j)/2] || i == j)
+
+   if (b == a[(i + j)/2]) printf("%d\n", (i + j)/2);
+   else printf("not found\n");
+
+   return 0;
+}
