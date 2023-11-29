f69403b2cfd4a0608d57b72460deddff46528246
Igor Dimitrov <igor-dimi@protonmail.com>
2023-11-29 13:52:19 +0100

    added gcd.cc

--- 02/binsearch.cc
+++ 02/binsearch.cc
@@ -11,15 +11,15 @@ int main()
 
     int i = 0;
     int j = n - 1;
-   //Invariant: b possibly in a[i ... j]
-   while (b != a[(i + j)/2] && i != j){
-       if (b > a[(i + j) / 2]) i = (i + j)/2 + 1;
-       else j = (i + j)/2 - 1;
-   }
-   //Post-condition: b possibly in a[i ... j] && (b == a[(i + j)/2] || i == j)
+    //Invariant: b possibly in a[i ... j]
+    while (b != a[(i + j)/2] && i != j){
+        if (b > a[(i + j) / 2]) i = (i + j)/2 + 1;
+        else j = (i + j)/2 - 1;
+    }
+    //Post-condition: b possibly in a[i ... j] && (b == a[(i + j)/2] || i == j)
 
-   if (b == a[(i + j)/2]) printf("%d\n", (i + j)/2);
-   else printf("%d\n", -1);
+    if (b == a[(i + j)/2]) printf("%d\n", (i + j)/2);
+    else printf("%d\n", -1);
 
-   return 0;
+    return 0;
 }
