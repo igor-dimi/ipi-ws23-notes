98ae613b4f508b7d90c351365354221ff40cd09b
Igor Dimitrov <igor-dimi@protonmail.com>
2024-01-17 13:05:58 +0100

    new workfiles 06

--- 06/list03.cc
+++ 06/list03.cc
@@ -1,4 +1,5 @@
 #include "../code/fcpp.hh"
+#include <stdio.h>
 
 // erzeugen einer leeren liste
 // einfuegen von elementen an beliebiger Stelle
@@ -43,5 +44,70 @@ void insert(List* l, ListEl* pos, ListEl* el)
         pos->succ = el;
         l->count++;
     }
+}
+
+ListEl* remove_list(List* l, ListEl* pos)
+{
+    ListEl* el;
+    if (pos == 0){ //remove from the beginning
+        el = l->head;
+        if (el != 0) {
+            l->head = l->head->succ;
+            l->count--;
+        }
+    }
+    else {
+        el = pos->succ;
+        if (el != 0) {
+            pos->succ = pos->succ->succ;
+            l->count--;
+        }
+    }
+    return el;
+}
+
+void print_list_el(ListEl* el)
+{
+    if (el == 0) return;
+    printf("%d", el->val);
+    printf("->");
+    print_list_el(el->succ);
+    // print(el->val);
+    // print("->");
+    // print_list_el(el->succ);
+    
+}
+
+void print_list(List* l)
+{
+    if (l->head == 0) return;
+    print_list_el(l->head);
+}
+
+int main()
+{
+    List l;
+    ListEl le1;
+    ListEl le2;
+    ListEl le3;
+    ListEl le4;
+    ListEl le5;
+    le1.val = 10;
+    le2.val = 13;
+    le3.val = 7;
+    le4.val = 20;
+    le5.val = 17;
+    make_empty_list(&l);
+    insert(&l, 0, &le1); 
+    insert(&l, 0, &le2);
+    insert(&l, 0, &le3);
+    insert(&l, 0, &le4);
+    ListEl* loc = find(l, 7);
+    insert(&l, loc, &le5);
+    ListEl* loc2 = find(l, 17);
+    remove_list(&l, loc2);
+    print_list(&l);
+    print("");
+    return 0;
 }
 
---
+++ 06/set03.cc
@@ -0,0 +1,158 @@
+#include "../code/fcpp.hh"
+#include <stdio.h>
+
+
+struct ListEl
+{
+    int val;
+    ListEl* succ;
+};
+
+struct List
+{
+    int count;
+    ListEl* head;
+};
+
+void make_empty_list(List* l)
+{
+    l->head = 0;
+    l->count = 0;
+}
+
+ListEl* find(List l, int val)
+{
+    ListEl* el = l.head;
+    while (el->val != val && el != 0) el = el->succ;
+    // el->val == val || el == 0 <=> el->val != val => el == 0;
+    return el;
+}
+
+void insert(List* l, ListEl* pos, ListEl* el)
+{
+    if (pos == 0){ //insert at the beginning of the list
+        el->succ = l->head;
+        l->head = el;
+        l->count++;
+    }
+    else {
+        el->succ = pos->succ;
+        pos->succ = el;
+        l->count++;
+    }
+}
+
+ListEl* remove_list(List* l, ListEl* pos)
+{
+    ListEl* el;
+    if (pos == 0){ //remove from the beginning
+        el = l->head;
+        if (el != 0) {
+            l->head = l->head->succ;
+            l->count--;
+        }
+    }
+    else {
+        el = pos->succ;
+        if (el != 0) {
+            pos->succ = pos->succ->succ;
+            l->count--;
+        }
+    }
+    return el;
+}
+
+void print_list_el(ListEl* el)
+{
+    if (el == 0) return;
+    printf("%d", el->val);
+    printf("->");
+    print_list_el(el->succ);
+    // print(el->val);
+    // print("->");
+    // print_list_el(el->succ);
+    
+}
+
+void print_list(List* l)
+{
+    if (l->head == 0) return;
+    print_list_el(l->head);
+}
+
+struct Set
+{
+    List list;
+};
+
+Set* make_empty_set()
+{
+    Set* s = new Set;
+    make_empty_list(&(s->list));
+    return s;
+}
+
+bool is_el(Set* s, int val)
+{
+    for (ListEl* el = s->list.head; el != 0; el = el->succ)
+        if (el->val == val) return true;
+    return false;
+}
+
+void insert_in_set(Set* s, int val)
+{
+    if (!is_el(s, val)){
+        ListEl* el = new ListEl;
+        el->val = val;
+        insert(&s->list, 0, el);
+    }
+}
+
+void remove_from_set(Set* s, int val)
+{
+    if (s->list.head == 0) return;
+    if (s->list.head->val == val){
+        ListEl* el = remove_list(&s->list, 0);
+        delete el;
+        return;
+    }
+    // Search in list, always test the successor
+    for (ListEl* el = s->list.head; el->succ != 0; el = el->succ)
+        if (el->succ->val == val){
+            ListEl* p = remove_list(&s->list, el);
+            delete p;
+            return;
+        }
+    
+}
+
+void print_set(Set* s)
+{
+    printf("{");
+    for (ListEl* el = s->list.head; el != 0; el = el->succ)
+        // print(el->val);
+        printf("%d, ", el->val);
+    printf("}");
+}
+
+
+
+int main()
+{
+    Set* s = make_empty_set();
+    insert_in_set(s, 10);
+    insert_in_set(s, 11);
+    insert_in_set(s, 20);
+    insert_in_set(s, 19);
+    print_set(s); 
+    print("");
+    remove_from_set(s, 11);
+    print_set(s);
+    print("");
+    for (int i = 3; i < 100000; i *= 5)
+        insert_in_set(s, (17137 * i) % 47);
+    print_set(s);
+    print("");
+
+
+}
